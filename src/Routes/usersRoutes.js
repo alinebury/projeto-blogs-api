b08a480c4f5622444684da25177e5668f9dadbee
Aline Bury <alinebury@gmail.com>
2022-08-08 18:12:12 -0300

    userRoutes

--- src/Routes/usersRoutes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const usersController = require('../controllers/usersController');
-
-const usersRoutes = new Router();
-
-usersRoutes.post('/login', usersController.add);
-
-module.exports = usersRoutes;
